b4e24a2ad9b3f140dd49c55bd0aa1828fa823409
Eugene Che <indexpro24@gmail.com>
2023-09-07 16:55:15 +0300

    с

--- project/src/UI/Information/v1/index.jsx
+++ project/src/UI/Information/v1/index.jsx
@@ -46,8 +46,7 @@ export default () => {
             Информация о продукции
           </h2>
           <p className="mb-8 font-light lg:text-xl">
-            Связаться с нами вы можете любым удобным для вас способом по
-            электронной почте, по телефону или личным визитом.
+            Подробная информация о продукции, областях применения и процессах изготовления
           </p>
         </div>
         <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16   sm:mt-16  lg:mx-0 lg:max-w-none lg:grid-cols-3">
